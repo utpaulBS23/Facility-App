2ff60ec9e0a9ed01f9293b3d27de58c7faab7d63
Md. Shahin Bashar <sbiitju@gmail.com>
2026-04-16 15:09:06 +0600

    feat: Update SVG icons and localization for improved UI consistency

--- lib/src/presentation/core/widgets/navigation_shell.dart
+++ lib/src/presentation/core/widgets/navigation_shell.dart
@@ -1,3 +1,4 @@
+import 'package:facility_management_app/src/presentation/core/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -19,30 +20,72 @@ class _NavigationShellState extends State<NavigationShell> {
     return Scaffold(
       body: widget.statefulNavigationShell,
       bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: context.color.primary,
+        iconSize: 28,
         currentIndex: widget.statefulNavigationShell.currentIndex,
         onTap: (index) {
           widget.statefulNavigationShell.goBranch(index);
         },
         items: [
           BottomNavigationBarItem(
-            icon: Assets.icons.shift.svg(),
-            label: context.locale.home,
+            icon: Assets.icons.shift.svg(width: 28, height: 28),
+            label: context.locale.shift,
+            activeIcon: Assets.icons.shift.svg(
+              width: 28,
+              height: 28,
+              colorFilter: ColorFilter.mode(
+                context.color.primary,
+                BlendMode.srcIn,
+              ),
+            ),
           ),
           BottomNavigationBarItem(
-            icon: Assets.icons.location.svg(),
+            icon: Assets.icons.attendance.svg(width: 28, height: 28),
             label: context.locale.attendance,
+            activeIcon: Assets.icons.attendance.svg(
+              width: 28,
+              height: 28,
+              colorFilter: ColorFilter.mode(
+                context.color.primary,
+                BlendMode.srcIn,
+              ),
+            ),
           ),
           BottomNavigationBarItem(
-            icon: Assets.icons.task.svg(),
-            label: context.locale.profile,
+            icon: Assets.icons.task.svg(width: 28, height: 28),
+            label: context.locale.task,
+            activeIcon: Assets.icons.task.svg(
+              width: 28,
+              height: 28,
+              colorFilter: ColorFilter.mode(
+                context.color.primary,
+                BlendMode.srcIn,
+              ),
+            ),
           ),
           BottomNavigationBarItem(
-            icon: Assets.icons.issue.svg(),
-            label: context.locale.profile,
+            icon: Assets.icons.issue.svg(width: 28, height: 28),
+            label: context.locale.issues,
+            activeIcon: Assets.icons.issue.svg(
+              width: 28,
+              height: 28,
+              colorFilter: ColorFilter.mode(
+                context.color.primary,
+                BlendMode.srcIn,
+              ),
+            ),
           ),
           BottomNavigationBarItem(
-            icon: Assets.icons.menu.svg(),
-            label: context.locale.profile,
+            icon: Assets.icons.menu.svg(width: 28, height: 28),
+            label: context.locale.menu,
+            activeIcon: Assets.icons.menu.svg(
+              width: 28,
+              height: 28,
+              colorFilter: ColorFilter.mode(
+                context.color.primary,
+                BlendMode.srcIn,
+              ),
+            ),
           ),
         ],
       ),
