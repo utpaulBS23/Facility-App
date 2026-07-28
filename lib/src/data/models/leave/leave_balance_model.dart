ff9b42cb05dd3b40a1ba5f0b9283e96a1da26b88
AMIR-BS1968 <amir.islam@brainstation-23.com>
2026-07-28 23:26:27 +0600

    feat: data layer implementation

---
+++ lib/src/data/models/leave/leave_balance_model.dart
@@ -0,0 +1,43 @@
+import 'package:dart_mappable/dart_mappable.dart';
+import 'leave_policy_model.dart';
+
+part 'leave_balance_model.mapper.dart';
+
+@MappableClass(generateMethods: GenerateMethods.decode)
+class LeaveBalanceModel with LeaveBalanceModelMappable {
+  const LeaveBalanceModel({
+    required this.id,
+    this.leavePolicy,
+    this.year,
+    this.allocatedDays,
+    this.usedDays,
+    this.carriedForwardDays,
+    this.pendingDays,
+    this.adjustedDays,
+    this.totalAvailableDays,
+    this.remainingDays,
+    this.notes,
+  });
+
+  final int id;
+  @MappableField(key: 'leave_policy')
+  final LeavePolicyModel? leavePolicy;
+  final int? year;
+  @MappableField(key: 'allocated_days', hook: StringToNullableDoubleHook())
+  final double? allocatedDays;
+  @MappableField(key: 'used_days', hook: StringToNullableDoubleHook())
+  final double? usedDays;
+  @MappableField(key: 'carried_forward_days', hook: StringToNullableDoubleHook())
+  final double? carriedForwardDays;
+  @MappableField(key: 'pending_days', hook: StringToNullableDoubleHook())
+  final double? pendingDays;
+  @MappableField(key: 'adjusted_days', hook: StringToNullableDoubleHook())
+  final double? adjustedDays;
+  @MappableField(key: 'total_available_days', hook: StringToNullableDoubleHook())
+  final double? totalAvailableDays;
+  @MappableField(key: 'remaining_days', hook: StringToNullableDoubleHook())
+  final double? remainingDays;
+  final String? notes;
+
+  static const fromJson = LeaveBalanceModelMapper.fromJson;
+}
