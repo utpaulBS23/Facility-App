9dfc601db58a1905e0f609d6b06bb836fc830289
UtpaulBS23 <utpaul.sarkar@brainstation-23.com>
2026-09-11 19:18:03 +0600

    fix(visit): visit model updated meta to stats

--- lib/src/data/models/visit_model.dart
+++ lib/src/data/models/visit_model.dart
@@ -6,15 +6,15 @@ part 'visit_model.mapper.dart';
 
 @MappableClass(generateMethods: GenerateMethods.decode)
 class VisitListResponseModel with VisitListResponseModelMappable {
-  VisitListResponseModel({required this.data, this.meta});
+  VisitListResponseModel({required this.data, this.stats});
 
   final List<VisitSummaryModel> data;
-  final VisitStatsModel? meta;
+  final VisitStatsModel? stats;
 
   static const fromJson = VisitListResponseModelMapper.fromJson;
 
   VisitListEntity toEntity() => VisitListEntity(
-        stats: meta?.toEntity(),
+        stats: stats?.toEntity(),
         visits: data.map((e) => e.toEntity()).toList(),
       );
 }
@@ -22,10 +22,21 @@ class VisitListResponseModel with VisitListResponseModelMappable {
 @MappableClass(generateMethods: GenerateMethods.decode)
 class VisitStatsModel with VisitStatsModelMappable {
   VisitStatsModel({
+    required this.total,
+    required this.pending,
+    required this.inProgress,
+    required this.completed,
     required this.todayCount,
     required this.thisWeekCount,
-    int? completed,
-  }) : completed = completed ?? 0;
+  });
+
+  final int total;
+  final int pending;
+
+  @MappableField(key: 'in_progress')
+  final int inProgress;
+
+  final int completed;
 
   @MappableField(key: 'today_count')
   final int todayCount;
@@ -33,15 +44,12 @@ class VisitStatsModel with VisitStatsModelMappable {
   @MappableField(key: 'this_week_count')
   final int thisWeekCount;
 
-  @MappableField(key: 'completed_count')
-  final int completed;
-
   static const fromJson = VisitStatsModelMapper.fromJson;
 
   VisitStatsSummaryEntity toEntity() => VisitStatsSummaryEntity(
         todayCount: todayCount,
         thisWeekCount: thisWeekCount,
-        inProgress: 0,
+        inProgress: inProgress,
         completed: completed,
       );
 }
